bf2523d94b412da9dd28c2ec2696c9ec58505f6f
Kashif Ali <technicalkashifmehrvi@gmail.com>
2025-07-12 20:34:21 +0500

    Update labbaik.sh

--- labbaik.sh
+++ labbaik.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+
 clear
-figlet -f slant "LABBAIK" | lolcat
+figlet -f slant "FAZE" | lolcat
 sleep 1
+
 echo -e "\e[1;32m🔄 Installing Required Packages...\e[0m"
 pkg update -y &> /dev/null
-pkg install python -y &> /dev/null
-pkg install git -y &> /dev/null
+pkg install -y python git curl figlet &> /dev/null
 pip install requests tqdm pycurl lolcat rich &> /dev/null
 
 clear
-echo -e "\e[1;34m🚀 Cloning Labbaik Tool...\e[0m"
+echo -e "\e[1;34m🚀 Cloning Maaz Tool from GitHub...\e[0m"
 rm -rf Labbaik-Tool
 git clone --depth=1 https://github.com/Labbaik757/Labbaik-Tool.git
-cd Labbaik-Tool
+cd Labbaik-Tool || { echo -e "\e[1;31m❌ Folder not found! Installation failed.\e[0m"; exit 1; }
 
 clear
-figlet "Maaz Tool" | lolcat
+figlet "MAAZ TOOL" | lolcat
 sleep 1
-echo -e "\e[1;32m⚡ Tool Installed Successfully!\e[0m"
+echo -e "\e[1;32m✅ Tool Installed Successfully!\e[0m"
 sleep 1
+
 xdg-open "https://wa.me/923079741690"
 sleep 2
 
